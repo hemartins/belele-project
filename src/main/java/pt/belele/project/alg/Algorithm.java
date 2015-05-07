602e2c7f20893855fb103d84d3e13437fe445057
linos <ee08282@fe.up.pt>
2015-05-08 00:30:08 +0100

    1st try

--- src/main/java/pt/belele/project/alg/Algorithm.java
+++ src/main/java/pt/belele/project/alg/Algorithm.java
@@ -1,5 +1,22 @@
 package pt.belele.project.alg;
 
-public class Algorithm {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Algorithm {
+	
+	Double lucro_total;
+	Integer numero_jornadas_disputadas;
+	List<Double> lucro_por_jornada = new ArrayList<Double>();
+	
+	public void calculoLucro(){
+
+		for(int i=1; i <= numero_jornadas_disputadas; i++)
+		lucro_total = lucro_total + lucro_por_jornada.get(i);
+	}
+	
+	public static void main(String [ ] args)
+	{
+	      
+	}
 }
